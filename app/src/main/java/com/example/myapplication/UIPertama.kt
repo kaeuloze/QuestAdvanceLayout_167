1818a2ffd1e59dc0316dc9fbb7fb2d2969c45242
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-10-24 10:29:51 +0700

    Menampilkan teks pertama (nama prodi)

--- app/src/main/java/com/example/myapplication/UIPertama.kt
+++ app/src/main/java/com/example/myapplication/UIPertama.kt
@@ -18,5 +18,17 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
+    Column(
+        modifier = Modifier
+            .padding (top = 100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 
-}
+    }
